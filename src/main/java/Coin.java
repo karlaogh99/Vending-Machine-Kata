0ec6c0a2e5f4bfbfa3041f9752d2623b87925898
Carlos <carlosgarciahernandez1999@gmail.com>
2022-01-16 12:58:53 +0000

    green: accept 5 cent coin

--- src/main/java/Coin.java
+++ src/main/java/Coin.java
@@ -1,4 +1,20 @@
 public class Coin {
+    private final double peso;
+    private final double diametro;
+
+
     public Coin(double diametro, double peso) {
+        this.diametro = diametro;
+        this.peso = peso;
     }
+
+    public double getDiametro() {
+        return diametro;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+
 }
--- src/main/java/CoinValidationSystem.java
+++ src/main/java/CoinValidationSystem.java
@@ -1,6 +1,9 @@
 public class CoinValidationSystem {
 
-    public boolean validate(Coin fiveCentCoin) {
+    public boolean validate(Coin coin) {
+        if (coin.getDiametro() == 21.25 && coin.getPeso() == 3.9){
+            return true;
+        }
         return false;
     }
 }
