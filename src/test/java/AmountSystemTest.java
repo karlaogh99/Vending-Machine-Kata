4a4b7494ee686a50c21561312ff366dd3e64f06e
Carlos <carlosgarciahernandez1999@gmail.com>
2022-01-17 16:40:15 +0000

    AmountSystemTest Refactor

--- src/test/java/AmountSystemTest.java
+++ src/test/java/AmountSystemTest.java
@@ -1,25 +1,26 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertTrue;
-import static org.junit.Assert.assertEquals;
 
 public class AmountSystemTest {
-
+    AmountSystem amountSystem;
+    double totalAmount;
+    Coin oneEuroCoin;
     @Before
     public void setup(){
-
+        this.amountSystem = new AmountSystem();
+        this.oneEuroCoin = new Coin(23.25, 7.5);
 
     }
     @Test
     public void add_the_amount_of_a_coin_if_is_accept(){
-        AmountSystem amountSystem = new AmountSystem();
-        Coin oneEuroCoin = new Coin(23.25, 7.5);
 
         double expectedAmount = 1.00;
         amountSystem.addAmountFrom(oneEuroCoin);
-        double totalAmount = amountSystem.getCurrentAccount();
-        assertTrue(totalAmount == expectedAmount);
+        this.totalAmount = amountSystem.getCurrentAccount();
+        assertEquals(totalAmount, expectedAmount);
 
     }
     @Test
