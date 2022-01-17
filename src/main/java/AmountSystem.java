65ec1dda1345657cd25edbd966f3282917099226
Carlos <carlosgarciahernandez1999@gmail.com>
2022-01-17 15:54:54 +0000

    red: add the amount of a coin if is accept

---
+++ src/main/java/AmountSystem.java
@@ -0,0 +1,12 @@
+
+public class AmountSystem {
+
+    public double getCurrentAccount() {
+        return 0;
+    }
+
+    public double addAmountFrom(Coin oneEuroCoin) {
+        return 0;
+
+    }
+}
---
+++ src/test/java/AmountSystemTest.java
@@ -0,0 +1,29 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+public class AmountSystemTest {
+
+    @Before
+    public void setup(){
+
+
+    }
+    @Test
+    public void add_the_amount_of_a_coin_if_is_accept(){
+        AmountSystem amountSystem = new AmountSystem();
+        Coin oneEuroCoin = new Coin(23.25, 2.125);
+
+        double expectedAmount = 1.00;
+        amountSystem.addAmountFrom(oneEuroCoin);
+        double totalAmount = amountSystem.getCurrentAccount();
+        assertTrue(totalAmount == expectedAmount);
+
+    }
+    @Test
+    public void add(){
+
+    }
+}
